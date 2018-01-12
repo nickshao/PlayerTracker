d1c48b961e14389354f7888180865529909e4ffb
y95847frank <b03902052@ntu.edu.tw>
2018-01-12 23:57:17 +0800

    add color

--- index2court/main.py
+++ index2court/main.py
@@ -9,7 +9,7 @@ import numpy as np
 import cv2
 
 color_class = pickle.load( open( "rgb_filter2_class.pkl", "rb" ) )
-color_type = ['#E13A3E', '#008348', '#061922', '#007DC5']
+color_type = [(51,51,255), (255,51,51), (51,255,51), (32,32,32)]
 
 matrix = np.load("transform.npy")
 
@@ -17,11 +17,23 @@ for i in range(4,80):
     fname = 'output{0:03d}'.format(i)
 
     data = pd.read_json('../yolo/jazz_thunder/out/output{0:03d}.json'.format(i))
+    '''
     ax = plt.axes(xlim=(Constant.X_MIN,Constant.X_MAX), ylim=(Constant.Y_MIN, Constant.Y_MAX))
     ax.axis('off')
     ax.grid(False)  # Remove grid
     #print(color_class[fname])
     #print(data['bottomright'])
+    '''
+    
+    img= cv2.imread('court.png')
+    for j in range(len(data['bottomright'])):
+        pts = np.array([[(data['bottomright'][j]['x']+data['topleft'][j]['x'])/2, data['bottomright'][j]['y']]], dtype = "float32")
+        pts = np.array([pts])
+        pts = cv2.perspectiveTransform(pts, matrix[i-4])
+        cv2.circle(img, (pts[0][0][0], pts[0][0][1]), 10, color_type[color_class[fname][j]], -1)
+    cv2.imwrite('game/myfig_{0:03d}.jpg'.format(i), img)
+    
+    '''
     player_circles = [plt.Circle((0, 0), Constant.PLAYER_CIRCLE_SIZE, color=color_type[color_class[fname][j]]) for (j,player) in enumerate(data['bottomright'])]
     for (j, circle) in enumerate(player_circles):
         pts = np.array([[data['bottomright'][j]['x'], data['bottomright'][j]['y']]], dtype = "float32")
@@ -37,4 +49,5 @@ for i in range(4,80):
     plt.show()
     plt.savefig('game/myfig_{0:03d}.jpg'.format(i))
     plt.close()
+    '''
     
