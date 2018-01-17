c2b7cb3b247b5aa5a9f3785e83ee4e0dce5f3299
Spicy30 <spicy21721313@gmail.com>
2018-01-18 01:14:11 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+wget https://pjreddie.com/media/files/yolo.weights -O bin/yolo.weights
 YOLO_PATH='yolo/darkflow'
-./${YOLO_PATH}/flow --imgdir input_frames/jazz_thunder/ --model ${YOLO_PATH}/cfg/yolo.cfg --load ${YOLO_PATH}/bin/yolo.weights --gpu 1.0
+./${YOLO_PATH}/flow --imgdir input_frames/jazz_thunder/ --model cfg/yolo.cfg --load bin/yolo.weights --gpu 1.0
 
